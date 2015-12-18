786169b6f097f8f39c1424fb9654f9136dcc760c
Mike Smith <mls40@cam.ac.uk>
2015-12-18 16:36:42 +0000

    Started adding alignment reading
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/IONiseR@111646 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_Fast5Summary.R
+++ tests/testthat/test_Fast5Summary.R
@@ -12,7 +12,21 @@ test_that("dim is not defined", {
   expect_equal(dim(s.typhi.rep2), NULL)
 })
 
-fast5file <- system.file('extdata', 'example.fast5', package = "IONiseR")
+fast5files <- system.file('extdata', c('example.fast5', 'example_v2.fast5'), package = "IONiseR")
+f1 <- readFast5Summary(fast5files)
+
 test_that("Can fast5 file be read?", {
-  expect_is(readFast5Summary(fast5file), 'Fast5Summary')
+  expect_is(f1, 'Fast5Summary')
+})
+
+test_that("acessors work", {
+    expect_is(readInfo(f1), 'data.frame')
+    expect_is(rawData(f1), 'data.frame')
+    expect_true(all(dim(rawData(f1)) == c(2,5)))
+    expect_is(baseCalled(f1), 'data.frame')
+    expect_is(fastq(f1), 'ShortReadQ')
+})
+
+test_that("Show method prints summary", {
+    expect_output(show(f1), regexp = "^Object of class: Fast5Summary")
 })
